775af149aecedd23fb0fc2e985b1537b2e36ca03
VictorCandelaria <victorcandelaria69@gmail.com>
2025-11-30 22:07:06 -0600

    Agregar archivos de proyecto.

---
+++ quisodecir/corrector.cpp
@@ -0,0 +1,82 @@
+/*****************************************************************************************************************
+    UNIVERSIDAD NACIONAL AUTONOMA DE MEXICO
+    FACULTAD DE ESTUDIOS SUPERIORES ARAGON
+
+    Computadoras y programación
+    (Candelaria Rosas Víctor Eduardo 323096537) Ponga su nombre y número de cuenta aquí
+
+    Implementación oficial compatible con el evaluador UNAM para el proyecto
+    "Usted quiso decir" (Diccionario, ClonaPalabras, ListaCandidatas)
+******************************************************************************************************************/
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "corrector.h"
+
+/* ============================================================
+   FUNCIONES INTERNAS
+   ============================================================ */
+
+static void aMinusculas(char* s)
+{
+    for (int i = 0; s[i]; i++)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            s[i] = s[i] + 32;
+    }
+}
+
+static int esSeparador(char c)
+{
+    return (c == ' ' || c == '\t' ||
+        c == '\n' || c == '\r' ||
+        c == ',' || c == ';' ||
+        c == '(' || c == ')');
+}
+
+
+static int esAlfabetico(char c)
+{
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+
+static void limpiarYValidar(char* tok)
+{
+    int escritura = 0;
+    for (int lectura = 0; tok[lectura]; lectura++)
+    {
+        if (esAlfabetico(tok[lectura]))
+            tok[escritura++] = tok[lectura];
+    }
+    tok[escritura] = '\0';
+}
+
+static int buscarBinaria(const char dic[][TAMTOKEN], int n, const char* pal)
+{
+    int lo = 0, hi = n - 1;
+    while (lo <= hi)
+    {
+        int mid = (lo + hi) / 2;
+        int cmp = strcmp(dic[mid], pal);
+        if (cmp == 0) return mid;
+        if (cmp < 0) lo = mid + 1;
+        else hi = mid - 1;
+    }
+    return -lo - 1;
+}
+
+static void insertarOrdenado(char dic[][TAMTOKEN], int est[], int& n, int pos, const char* pal)
+{
+    for (int i = n; i > pos; i--)
+    {
+        strcpy(dic[i], dic[i - 1]);
+        est[i] = est[i - 1];
+    }
+    strcpy(dic[pos], pal);
+    est[pos] = 1;
+    n++;
+}
